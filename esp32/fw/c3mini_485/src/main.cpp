02a4e85d13275229c0c853d921c5430c066c27e1
iv <ivk2004@bk.ru>
2024-11-04 07:33:10 +0300

    ota added

--- esp32/fw/c3mini_485/src/main.cpp
+++ esp32/fw/c3mini_485/src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+   })
+    .onEnd([]() {
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+    })
+    .onError([](ota_error_t error) {
+      if (error == OTA_AUTH_ERROR) {
+        
+      } else if (error == OTA_BEGIN_ERROR) {
+        
+      } else if (error == OTA_CONNECT_ERROR) {
+        
+      } else if (error == OTA_RECEIVE_ERROR) {
+        
+      } else if (error == OTA_END_ERROR) {
+        
+      }
+    });
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    ArduinoOTA.handle();
 }
